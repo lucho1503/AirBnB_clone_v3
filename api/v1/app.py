45efb9759074551deeef4e2b9f9d350482423c90
lucho1503 <lmmc.1503@gmail.com>
2020-04-30 02:54:19 +0000

    fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,6 +12,8 @@ host = os.getenv("HBNB_API_HOST", "0.0.0.0")
 port = os.getenv("HBNB_API_PORT", 5000)
 
 app.register_blueprint(app_views)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
 
 @app.teardown_appcontext
 def close_app(exception):
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@
 from api.v1.views import app_views
 from flask import jsonify, request
 
+
 @app_views.route('/status', methods=['GET'])
 def status():
     """ return a json status """
