194749a2a737de86960ffe167216d712fecc16d3
lucho1503 <lmmc.1503@gmail.com>
2020-04-30 03:36:53 +0000

    index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,7 @@
 
 from api.v1.views import app_views
 from flask import jsonify, request
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
@@ -10,3 +11,15 @@ def status():
     """ return a json status """
     if request.method == 'GET':
         return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """ retrieves the number of eacj object by type """
+    if request.method == 'GET':
+        res = {}
+        classes = {"Amenity": "amenities", "City": "cities", "Place": "places",
+                   "Review": "reviews", "State": "states", "User": "users"}
+        for k, v in classes.items():
+            res[v] = storage.count(k)
+        return jsonify(res)
