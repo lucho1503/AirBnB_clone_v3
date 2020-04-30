fb0bfabb58fb389dcf23352c4e3ae7b4482381ca
lucho1503 <lmmc.1503@gmail.com>
2020-04-30 03:20:35 +0000

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,9 +7,10 @@ from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
-host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-port = os.getenv("HBNB_API_PORT", 5000)
+host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+port = os.getenv('HBNB_API_PORT', 5000)
 
 app.register_blueprint(app_views)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
