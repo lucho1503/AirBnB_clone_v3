88ea03571e4670f1de56c214e9ca0575756665aa
lucho1503 <lmmc.1503@gmail.com>
2020-04-30 03:40:06 +0000

    fixed

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,7 +13,7 @@ host = os.getenv('HBNB_API_HOST', '0.0.0.0')
 port = os.getenv('HBNB_API_PORT', 5000)
 
 app.register_blueprint(app_views)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+#app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 
 @app.teardown_appcontext
