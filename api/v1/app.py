d2e28dca4045a84e0a74a54898ae9f7403123461
lucho1503 <lmmc.1503@gmail.com>
2020-05-01 03:54:35 +0000

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,6 +6,7 @@ from flask import Flask, Blueprint, jsonify, abort
 from models import storage
 from api.v1.views import app_views
 from werkzeug.exceptions import HTTPException
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -15,7 +16,7 @@ port = os.getenv('HBNB_API_PORT', 5000)
 
 app.register_blueprint(app_views)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-
+cors = CORS(app, resources={r'/*': {'origin': host}})
 
 @app.teardown_appcontext
 def close_app(exception):
